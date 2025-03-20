b2f4f00d05c3803fe9721ba3c653e2da0161f662
Mohameddev73 <takhlitasimo@gmail.com>
2025-03-20 15:38:40 +0100

    change configue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Assure-toi que l'output est bien défini
+    sourcemap: false, // Désactive les sourcemaps pour éviter des erreurs de taille
+  }
 })
